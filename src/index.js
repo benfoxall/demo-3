4416c9d8ea5beb0c3112a2f62ea203436d672964
Ben Foxall <benfoxall@gmail.com>
2021-01-13 20:22:05 +0000

    !!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 const chalkAnimation = require("chalk-animation");
 const CFonts = require("cfonts");
 
-const prettyFont = CFonts.render("Demo 3");
+const prettyFont = CFonts.render("Demo 3!!");
 
 chalkAnimation.rainbow(prettyFont.string);
